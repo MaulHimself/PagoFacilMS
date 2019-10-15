4c0256f70e32672376d484bd2cd441bd0a163110
Mauricio Cid <mauricio@bucstudio.cl>
2019-10-15 17:23:57 -0300

    Initial commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApiController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function pay($id)
+    {
+        return 'bla';
+    }
+}
